7af67a6947dfbe5d93f3368338bb4fbc71a462d1
MehrdadFalahati <m.falahati2022@gmail.com>
2023-07-14 20:22:10 +0330

    first commit

---
+++ src/test/java/com/mehrdad/falahati/urlshorteningsystem/UrlShorteningSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mehrdad.falahati.urlshorteningsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShorteningSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
