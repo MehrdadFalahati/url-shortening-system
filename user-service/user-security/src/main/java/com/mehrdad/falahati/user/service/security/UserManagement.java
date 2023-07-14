a1292295c5d6e35564b5ef56abebfeea4f3d2c8a
MehrdadFalahati <m.falahati2022@gmail.com>
2023-07-14 23:15:30 +0330

    feat: implemented user security module.

---
+++ user-service/user-security/src/main/java/com/mehrdad/falahati/user/service/security/UserManagement.java
@@ -0,0 +1,22 @@
+package com.mehrdad.falahati.user.service.security;
+
+import com.mehrdad.falahati.user.service.domain.entity.User;
+import com.mehrdad.falahati.user.service.domain.port.input.service.UserApplicationService;
+import com.mehrdad.falahati.user.service.security.entity.UserSecurityEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserManagement implements UserDetailsService {
+
+    private final UserApplicationService userApplicationService;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userApplicationService.getUserByUsername(username);
+        return new UserSecurityEntity(user);
+    }
+}
---
+++ user-service/user-security/src/main/java/com/mehrdad/falahati/user/service/security/filter/JwtTokenFilter.java
@@ -0,0 +1,62 @@
+package com.mehrdad.falahati.user.service.security.filter;
+
+import com.mehrdad.falahati.user.service.security.UserManagement;
+import com.mehrdad.falahati.user.service.security.util.JwtTokenUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    private final JwtTokenUtil jwtTokenUtil;
+    private final UserManagement userManagement;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (header == null || header.isEmpty() || !header.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // Get jwt token and validate
+        final String token = header.split(" ")[1].trim();
+        if (!jwtTokenUtil.validate(token)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // Get user identity and set it on the spring security context
+        UserDetails userDetails = userManagement
+                .loadUserByUsername(jwtTokenUtil.getUsername(token));
+
+        UsernamePasswordAuthenticationToken
+                authentication = new UsernamePasswordAuthenticationToken(
+                userDetails, null,
+                userDetails == null ?
+                        List.of() : userDetails.getAuthorities()
+        );
+
+        authentication.setDetails(
+                new WebAuthenticationDetailsSource().buildDetails(request)
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        filterChain.doFilter(request, response);
+    }
+}
